781000d188da38b0954c12259c8470a007dfa464
Elena <lazarenko@ucu.edu.ua>
2018-12-07 10:31:30 +0200

    Next commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,18 +10,37 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Rose rose = Rose.getRandomRose();
-        System.out.println(rose);
-        Chamomile chamomile = Chamomile.getRandomChamolite();
-        System.out.println(chamomile);
-        Tulip tulip = Tulip.getRandomTulip();
-        System.out.println(tulip);
-        Basket basket = Basket.getRandomBasket();
-        System.out.println(basket);
-        Bouquet bouquet = Bouquet.getRandomBouquet();
-        System.out.println(bouquet);
-        Storage storage = new Storage();
-        storage.addRandomData();
-        System.out.println(storage);
+        System.out.println("Hello! You have entered Flower Shop 'Oles & Viktor Production'.");
+        System.out.println("Choose any option if you want to make an order: ");
+        int type = Integer.parseInt(System.console().readLine());
+        System.out.println("1. Order default bouquet \n 2. Make my own bouquet");
+        if (type == 1){
+            System.out.println("You can choose such bouquets: ");
+            System.out.println("1. Hundred roses \n 2. Hundred chamomiles \n 3. Hundred tulips \n 4. Hundred flowers from roses, tulips and chamomiles");
+            int bouquet = Integer.parseInt(System.console().readLine());
+        }
+        if (type == 2){
+            System.out.println("How much roses do you want to add?");
+            int nroses = Integer.parseInt(System.console().readLine());
+            System.out.println("How much chamomiles do you want to add?");
+            int nchamomiles = Integer.parseInt(System.console().readLine());
+            System.out.println("How much tulips do you want to add?");
+            int ntulips = Integer.parseInt(System.console().readLine());
+        }
+
+
+//        Rose rose = Rose.getRandomRose();
+//        System.out.println(rose);
+//        Chamomile chamomile = Chamomile.getRandomChamolite();
+//        System.out.println(chamomile);
+//        Tulip tulip = Tulip.getRandomTulip();
+//        System.out.println(tulip);
+//        Basket basket = Basket.getRandomBasket();
+//        System.out.println(basket);
+//        Bouquet bouquet = Bouquet.getRandomBouquet();
+//        System.out.println(bouquet);
+//        Storage storage = new Storage();
+//        storage.addRandomData();
+//        System.out.println(storage);
     }
 }
