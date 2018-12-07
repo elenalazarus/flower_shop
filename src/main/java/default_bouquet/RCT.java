ac95aa276a29ca8b711f43412f8deff2aedde8c1
Elena <lazarenko@ucu.edu.ua>
2018-12-07 09:54:43 +0200

    Next commit

--- src/main/java/default_bouquet/RCT.java
+++ src/main/java/default_bouquet/RCT.java
@@ -1,31 +1,37 @@
 
+
 package default_bouquet;
 
 import base.Color;
+import flower.Chamomile;
 import flower.Flower;
+import flower.Rose;
 import flower.Tulip;
 import set.FlowerList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class BouquetTulip extends FlowerList {
-    public BouquetTulip() {
+public class RCT extends FlowerList {
+    public RCT() {
     }
 
-    public BouquetTulip(List<Flower> initialFlowers) {
+    public RCT(List<Flower> initialFlowers) {
         super(initialFlowers);
     }
 
-    public static BouquetTulip HundredTulips() {
+    public static RCT HundredRCT() {
         List<Flower> flowers = new LinkedList();
         Color color = Flower.getRandomColor();
         int length = Flower.getRandomLength();
         float price = Flower.getRandomPrice();
 
-        for(int i = 0; i < 100; ++i) {
+        for(int i = 0; i < 33; ++i) {
             flowers.add(new Tulip(color, price, length));
+            flowers.add(new Chamomile(color, price, length));
+            flowers.add(new Rose(color, price, length));
         }
+        flowers.add(new Rose(color, price, length));
 
-        return new BouquetTulip(flowers);
+        return new RCT(flowers);
     }
 }
